a44e2070fe53f32040f82cb9385eb2e8cdd71c11
kwikoff100 <48369203+kwikoff100@users.noreply.github.com>
2019-04-14 00:20:00 -0400

    User Group Code Return

---
+++ TrackDatCash/app/src/main/java/com/example/trackdatcash/GView2Activity.java
@@ -0,0 +1,13 @@
+package com.example.trackdatcash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GView2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gview2);
+    }
+}
